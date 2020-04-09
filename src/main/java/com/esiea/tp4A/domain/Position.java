1b2811d14036869eea9fa9740d1bd55878d3debf
mohamed95870 <bouchiba@et.esiea.fr>
2020-04-09 15:23:13 +0200

    Update Position

--- src/main/java/com/esiea/tp4A/domain/Position.java
+++ src/main/java/com/esiea/tp4A/domain/Position.java
@@ -36,36 +36,5 @@ public interface Position {
         public Direction getDirection() {
             return direction;
         }
-
-        @Override
-        public Position forward() {
-            switch (direction) {
-                case NORTH:
-                    return Position.of(x, y+1, Direction.NORTH);
-                case SOUTH:
-                    return Position.of(x, y-1, Direction.SOUTH));
-                case WEST:
-                    return Position.of(x-1, y, Direction.WEST));
-                case EAST:
-                    return Position.of(x+1, y, Direction.EAST));
-            }
-            throw new IllegalStateException("Pas de direction: " + direction);
-        }
-
-        @Override
-        public Position backward() {
-            return Position.of(x, y-1, direction);
-        }
-
-        @Override
-        public Position left() {
-            return Position.of(x-1, y, direction);
-        }
-
-        @Override
-        public Position right() {
-            return Position.of(x+1, y, direction);
-        }
-
     }
 }
