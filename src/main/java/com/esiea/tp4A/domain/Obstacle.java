6dde5c8f2e899207f2803128b52b790fe4c9070f
mohamed95870 <bouchiba@et.esiea.fr>
2020-04-09 15:08:31 +0200

    Update Obstacle

--- src/main/java/com/esiea/tp4A/domain/Obstacle.java
+++ src/main/java/com/esiea/tp4A/domain/Obstacle.java
@@ -1,10 +1,10 @@
 package com.esiea.tp4A.domain;
 
-
 import java.util.Set;
+import java.util.HashSet;
 
 public class Obstacle {
-        public Set<Position> obstacles;
+        public Set<Position> obstacles = new HashSet<>();
         public final int positionMaxX;
         public final int positionMaxY;
         public final int positionMinX;
@@ -15,13 +15,29 @@ public class Obstacle {
             this.positionMaxY = 50;
             this.positionMinX = -49;
             this.positionMinY = -49;
+            this.obstaclesGenerate(obstaclesNumber);
 
+        }
+
+        public void obstaclesGenerate(int number) {
+            if(number >= 0 ) {
+                while (obstacles.size() != number) {
+                    Position obstacle = uniqueObstacleGenerate();
+                    obstacles.add(obstacle);
+                }
+            }
+        }
+
+        private Position uniqueObstacleGenerate(){
+            finalRandom finalrandom = new finalRandom();
+            Point p = new Point(finalrandom.getRandomInt(positionMaxX,positionMinX),finalrandom.getRandomInt(positionMaxY,positionMinY),this);
+            return new Position.FixedPosition(p.getPositionX(),p.getpositionY(),Direction.NORTH);
         }
 
         public boolean obstacleDetection(int x, int y) {
-            return obstacles.stream()
-                .anyMatch(p -> p.getX() == x && p.getY() == y);
-        }
+                return obstacles.stream()
+                    .anyMatch(p -> p.getX() == x && p.getY() == y);
+            }
 
         public void obstacleBreak(int x, int y) {
             obstacles.removeIf(position -> position.getX() == x && position.getY() == y);
