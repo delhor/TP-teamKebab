28cd0fa683c9f3f9fc509d929e39d492405d532e
hseck <48480043+hseck@users.noreply.github.com>
2020-04-12 17:35:24 +0200

    Update Direction and add Rotation

--- src/main/java/com/esiea/tp4A/domain/Direction.java
+++ src/main/java/com/esiea/tp4A/domain/Direction.java
@@ -1,6 +1,44 @@
 package com.esiea.tp4A.domain;
 
-public enum Direction {
-
-    NORTH, EAST, SOUTH, WEST;
+public enum Direction implements Rotation {
+    NORTH {
+        @Override
+        public Direction turnLeft() {
+            return WEST;
+        }
+        @Override
+        public Direction turnRight() {
+            return EAST;
+        }
+    },
+    SOUTH {
+        @Override
+        public Direction turnLeft() {
+            return EAST;
+        }
+        @Override
+        public Direction turnRight() {
+            return WEST;
+        }
+    },
+    EAST {
+        @Override
+        public Direction turnLeft() {
+            return NORTH;
+        }
+        @Override
+        public Direction turnRight() {
+            return SOUTH;
+        }
+    },
+    WEST {
+        @Override
+        public Direction turnLeft() {
+            return SOUTH;
+        }
+        @Override
+        public Direction turnRight() {
+            return NORTH;
+        }
+    }
 }
---
+++ src/main/java/com/esiea/tp4A/domain/Rotation.java
@@ -0,0 +1,7 @@
+package com.esiea.tp4A.domain;
+
+public interface Rotation {
+    Direction turnLeft();
+    Direction turnRight();
+
+}
