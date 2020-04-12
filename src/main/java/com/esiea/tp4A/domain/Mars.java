de94ddb27930e4aebbb55bad0333f5cc9f192730
mohamed95870 <bouchiba@et.esiea.fr>
2020-04-12 18:41:36 +0200

    Add Mars and Update Point

---
+++ src/main/java/com/esiea/tp4A/domain/Mars.java
@@ -0,0 +1,58 @@
+package com.esiea.tp4A.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class Mars implements PlanetMap {
+    private final PlanetMapImpl mapImpl;
+    private Set<Position> obstacles = new HashSet<>();
+    private Set<Rover> rovers = new HashSet<>();
+    public int number;
+    public Mars(int numberOfObstacles, int mapSize) {
+        mapImpl = new PlanetMapImpl(mapSize/2, -1*((mapSize/2) - 1), mapSize/2, -1*((mapSize/2) - 1));
+        this.generateObstacles(numberOfObstacles);
+    }
+    public void generateObstacles(int number) {
+        if(number >= 0 ) {
+            while (obstacles.size() != number) {
+                Position obstacle = uniqueObstacleGenerate();
+                obstacles.add(obstacle);
+            }
+        }
+    }
+    private Position uniqueObstacleGenerate(){
+        FinalRandom shared = new FinalRandom();
+        Point point;
+        do {
+            point = new Point(shared.getRandomInt(mapImpl.getMaxPositionX(), mapImpl.getMinPositionX()),shared.getRandomInt(mapImpl.getMaxPositionY(), mapImpl.getMinPositionY()),this);
+        } while(detectObstacle(point.getPositionX(), point.getPositionY()));
+        return new Position.FixedPosition(point.getPositionX(),point.getPositionY(),Direction.NORTH);
+    }
+    public boolean detectObstacle(int x, int y) {
+        return Stream.concat(rovers.stream().map(Rover::getPosition),
+            obstacles.stream())
+            .anyMatch(p -> p.getX() == x && p.getY() == y);
+    }
+    public void killObstacle(int x, int y) {
+        obstacles.removeIf(position -> position.getX() == x && position.getY() == y);
+        rovers.removeIf(rover -> rover.getPosition().getX() == x && rover.getPosition().getY() == y);
+    }
+    public void generateRovers() {
+        FinalRandom commons = new FinalRandom();
+        for (int i = 0; i < 50 ; i++) {
+            Point p = new Point(commons.getRandomInt(mapImpl.getMaxPositionX(), mapImpl.getMinPositionX()) , commons.getRandomInt(mapImpl.getMaxPositionY(), mapImpl.getMinPositionY()), this);
+            addRover(new Rover(new PositionImpl(p, Direction.NORTH), this, i));
+        }
+    }
+    public boolean addRover(Rover rover) {return rovers.add(rover);}
+
+    public Set<Rover> getRovers() {return rovers;}
+
+    @Override
+    public Set<Position> obstaclePositions() {return obstacles;}
+
+    public PlanetMapImpl getMapImpl() {return mapImpl;}
+
+    public void setObstacles(Set<Position> obstacles) {this.obstacles = obstacles;}
+}
---
+++ src/main/java/com/esiea/tp4A/domain/Point.java
@@ -0,0 +1,57 @@
+package com.esiea.tp4A.domain;
+
+public class Point {
+    private int positionX;
+    private int positionY;
+    private final Mars mapImpl;
+    public Point(int positionX, int positionY, Mars mars){
+        this.positionX = positionX; this.positionY = positionY; this.mapImpl = mars;
+    }
+    public boolean positionXForward(){
+        int positionTest = getPositionXForward();
+        if(!mapImpl.detectObstacle(positionTest,positionY)) {
+            positionX = positionTest;
+            return true;
+        }
+        return false;
+    }
+    public boolean positionXBackward(){
+        int positionTest = getPositionXBackward();
+        if(!mapImpl.detectObstacle(positionTest,positionY)) {
+            positionX = positionTest;
+            return true;
+        }
+        return false;
+    }
+    public boolean positionYForward(){
+        int positionTest = getPositionYForward();
+        if(!mapImpl.detectObstacle(positionX ,positionTest)) {
+            positionY = positionTest;
+            return true;
+        }
+        return false;
+    }
+    public boolean positionYBackward(){
+        int positionTest = getPositionYBackward();
+        if(!mapImpl.detectObstacle(positionX ,positionTest)) {
+            positionY = positionTest;
+            return true;
+        }
+        return false;
+    }
+    public int getPositionXForward(){
+        return (positionX + 1) > mapImpl.getMapImpl().getMaxPositionX() ? mapImpl.getMapImpl().getMinPositionX() : positionX + 1;
+    }
+    public int getPositionXBackward(){
+        return (positionX - 1) < mapImpl.getMapImpl().getMinPositionX() ? mapImpl.getMapImpl().getMaxPositionX() : positionX - 1;
+    }
+    public int getPositionYForward(){
+        return (positionY + 1) > mapImpl.getMapImpl().getMaxPositionY() ? mapImpl.getMapImpl().getMinPositionY() : positionY + 1;
+    }
+    public int getPositionYBackward(){
+        return (positionY - 1) < mapImpl.getMapImpl().getMinPositionY() ? mapImpl.getMapImpl().getMaxPositionY() : positionY - 1;
+    }
+    public int getPositionY() { return positionY; }
+    public int getPositionX() { return positionX; }
+}
+
